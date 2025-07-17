01e54f9a61743f4f07fd7cd90f10dfe5a631e27c
cuaderno-digital <oficina247maquina1@gmail.com>
2025-07-17 17:56:26 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,4 +3,10 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = "https://ueiiyibsnmapzygpmeza.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlaWl5aWJzbm1hcHp5Z3BtZXphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzODc2NzMsImV4cCI6MjA2Njk2MzY3M30.8yDCfUXBhqeEyFvPL5Pz7W5WWlbK4N25UOEXx4n1hdo";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        'usuario_id': localStorage.getItem('usuario_id') || ''
+      }
+    }
+  });
